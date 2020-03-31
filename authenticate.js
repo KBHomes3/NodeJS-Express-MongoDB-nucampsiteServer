aefbefa31d6d5b4711d9a20cb2fbf80c1f40c650
Alex Aucestovar <aucestovara@gmail.com>
2020-03-30 19:16:55 -0500

    Added Passport Facebook OAuth

--- authenticate.js
+++ authenticate.js
@@ -5,6 +5,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('./models/user');
 const jwt = require('jsonwebtoken');
 const config = require('./config.js');
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 exports.getToken = user => {
     return jwt.sign(user, config.secretKey, {expiresIn: 3600});
@@ -26,6 +27,37 @@ exports.jwtPassport = passport.use(
                     return done(null, user);
                 } else {
                     return done(null, false);
+                }
+            });
+        }
+    )
+);
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        }, 
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
                 }
             });
         }
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '2694766620759517',
+        clientSecret: '54b2fef3dcc79dc665f32533bbe24a39'
+    }
 }
